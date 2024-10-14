7b1ca9d3d1784325d576c9011682b0d3b166ef24
Arda <arda_isitan55@hotmail.com>
2024-10-14 21:34:17 +0300

    Added FavViewModel.

---
+++ app/src/main/java/com/example/foodorder/ui/viewmodel/FavViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.foodorder.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+//Favorilerle ilgili yemeklerle ilgil iişemler burda yapılacak.Bu kısmı en son yapacağım.
+class FavViewModel : ViewModel()
