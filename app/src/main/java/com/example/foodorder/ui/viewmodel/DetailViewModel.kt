3db8adf5c6d9559480c913fab7a8b7ca7a3cceb1
Arda <arda_isitan55@hotmail.com>
2024-10-14 21:34:27 +0300

    Added DetailViewModel.

---
+++ app/src/main/java/com/example/foodorder/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.foodorder.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+// Bu viewmodelda,öncelikle seçilen adete göre yemek ekleme işlemini yapacağım.
+// İlgili apiye yemek ekleme isteği yollanır.
+class DetailViewModel : ViewModel()
