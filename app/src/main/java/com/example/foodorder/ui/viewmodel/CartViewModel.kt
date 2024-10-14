f2281beda97a2e32ba3a3050579f9c3f907091d8
Arda <arda_isitan55@hotmail.com>
2024-10-14 21:34:35 +0300

    Added CartViewModel.

---
+++ app/src/main/java/com/example/foodorder/ui/viewmodel/CartViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.foodorder.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+// Bu viewmodelde,sepetteki yemeklerle ilgili işlemler yapılacak.
+// Sepetteki yemekleri getir apisi burda kullanılacak.
+// Burda listelenen yemeklerden istenilen ürünün silme işlemi yapılacak.
+// İstenilen adeti silme şeklinde de yapabilirsin.
+class CartViewModel : ViewModel()
