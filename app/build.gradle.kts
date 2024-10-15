131412e15a96aadfd124a209efe2cf34f065bf68
Arda <arda_isitan55@hotmail.com>
2024-10-15 11:33:13 +0300

    Added Dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,17 @@ android {
 }
 
 dependencies {
+    // ktx
+    implementation ("androidx.activity:activity-ktx:1.9.2")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.6")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
