c00dfd1409c5cdf6300723726810dac3d282f634
Arda <arda_isitan55@hotmail.com>
2024-10-19 13:00:17 +0300

    Added Api.

--- app/src/main/java/com/example/foodorder/retrofit/FoodsApi.kt
+++ app/src/main/java/com/example/foodorder/retrofit/FoodsApi.kt
@@ -1,10 +1,13 @@
 package com.example.foodorder.retrofit
 
 import com.example.foodorder.data.entity.CRUDResponse
+import com.example.foodorder.data.entity.CartFoodsResponse
 import com.example.foodorder.data.entity.FoodsResponse
+import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.PUT
 
 // URL : http://kasimadalan.pe.hu/yemekler/tumYemekleriGetir.php
@@ -13,7 +16,26 @@ interface FoodsApi {
     @GET("yemekler/tumYemekleriGetir.php")
     suspend fun getAllFoods(): FoodsResponse
 
+    @POST("yemekler/sepeteYemekEkle.php")
+    @FormUrlEncoded
+    suspend fun addToCart(
+        @Field("yemek_adi") yemek_adi: String,
+        @Field("yemek_resim_adi") yemek_resim_adi: String,
+        @Field("yemek_fiyat") yemek_fiyat: Int,
+        @Field("yemek_siparis_adet") yemek_siparis_adet: Int,
+        @Field("kullanici_adi") kullanici_adi: String
+    ): CRUDResponse
 
+    @POST("yemekler/sepettekiYemekleriGetir.php")
+    @FormUrlEncoded
+    suspend fun getFoodsInCart(
+        @Field("kullanici_adi") kullanici_adi: String
+    ): Response<CartFoodsResponse>
 
+    @POST("yemekler/sepettenYemekSil.php")
+    @FormUrlEncoded
+    suspend fun deleteFromCart(
+        @Field("sepet_yemek_id") sepet_yemek_id: Int, @Field("kullanici_adi") kullanici_adi: String
+    ): CRUDResponse
 
 }
