fd638417a78c49945b532d6cdb4eab4148cdf08e
Arda <arda_isitan55@hotmail.com>
2024-10-22 11:19:22 +0300

    Added SearchView for searching foods in ListFragment

--- app/src/main/java/com/example/foodorder/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/example/foodorder/ui/fragments/ListFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.foodorder.databinding.FragmentListBinding
 import com.example.foodorder.ui.adapter.FoodListAdapter
 import com.example.foodorder.ui.viewmodel.ListViewModel
+import com.google.android.material.search.SearchBar
+import com.google.android.material.search.SearchView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,19 +25,43 @@ class ListFragment : Fragment() {
         binding = FragmentListBinding.inflate(inflater, container, false)
 
 
-
         listViewModel.foodsList.observe(viewLifecycleOwner) { foodList ->
             val foodsAdapter = FoodListAdapter(foodList)
-            binding.rv.adapter = foodsAdapter
-            binding.rv.layoutManager =
-                StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            with(binding) {
+                rv.adapter = foodsAdapter
+                rv.layoutManager =
+                    StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                noData.isVisible = foodList.isEmpty()
+            }
         }
 
+        binding.searchView.setOnQueryTextListener(object :
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    listViewModel.filterFoods(it)
+                    binding.searchView.clearFocus()
+                }
+                return true
+            }
 
-
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let {
+                    listViewModel.filterFoods(it)
+                }
+                return true
+            }
+        })
 
         return binding.root
 
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.searchView.setQuery("", false)
+        binding.searchView.clearFocus()
 
     }
 
