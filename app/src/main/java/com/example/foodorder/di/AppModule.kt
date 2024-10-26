27b0e511508785fa495750527beddc139bbda9a1
Arda <arda_isitan55@hotmail.com>
2024-10-26 20:23:33 +0300

    Provided dependencies.

--- app/src/main/java/com/example/foodorder/di/AppModule.kt
+++ app/src/main/java/com/example/foodorder/di/AppModule.kt
@@ -1,13 +1,19 @@
 package com.example.foodorder.di
 
 
+import android.content.Context
+import androidx.room.Room
+import com.example.foodorder.data.datasource.localdatasource.LocalFoodDataSource
 import com.example.foodorder.data.datasource.remotedatasource.RemoteFoodDataSource
 import com.example.foodorder.data.repository.FoodRepository
 import com.example.foodorder.retrofit.FoodsApi
 import com.example.foodorder.retrofit.RetrofitInstance
+import com.example.foodorder.room.DataBase
+import com.example.foodorder.room.FavDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -29,8 +35,23 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRepository(foodsDataSource: RemoteFoodDataSource): FoodRepository {
-        return FoodRepository(foodsDataSource)
+    fun provideFavDao(@ApplicationContext context: Context): FavDao {
+        val db = Room.databaseBuilder(context, DataBase::class.java, "fav_db").build()
+        return db.getFavDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalDataSource(favDao: FavDao): LocalFoodDataSource {
+        return LocalFoodDataSource(favDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(
+        remoteFoodsDataSource: RemoteFoodDataSource, localFoodsDataSource: LocalFoodDataSource
+    ): FoodRepository {
+        return FoodRepository(remoteFoodsDataSource, localFoodsDataSource)
     }
 
 
