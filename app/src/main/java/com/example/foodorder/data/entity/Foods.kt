d985a84d5ca60e3dc69e3aa97698bd586aa6241c
Arda <arda_isitan55@hotmail.com>
2024-10-26 20:22:16 +0300

    Updated entity class for Room.

--- app/src/main/java/com/example/foodorder/data/entity/Foods.kt
+++ app/src/main/java/com/example/foodorder/data/entity/Foods.kt
@@ -1,10 +1,14 @@
 package com.example.foodorder.data.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.io.Serializable
 
+@Entity(tableName = "Foods")
 data class Foods(
-    val yemek_id: Int,
-    val yemek_adi: String,
-    val yemek_resim_adi: String,
-    val yemek_fiyat: Int
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "food_id") val yemek_id: Int,
+    @ColumnInfo(name = "food_name") val yemek_adi: String,
+    @ColumnInfo(name = "food_image_name") val yemek_resim_adi: String,
+    @ColumnInfo(name = "food_price") val yemek_fiyat: Int,
 ) : Serializable
