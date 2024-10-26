60bebc74a642ff946fcec045dd2408033d9ef097
Arda <arda_isitan55@hotmail.com>
2024-10-26 20:15:13 +0300

    Added LocalFoodDataSource.

--- app/src/main/java/com/example/foodorder/data/datasource/localdatasource/LocalFoodDataSource.kt
+++ app/src/main/java/com/example/foodorder/data/datasource/localdatasource/LocalFoodDataSource.kt
@@ -1,4 +1,17 @@
 package com.example.foodorder.data.datasource.localdatasource
 
-class LocalFoodDataSource {
+import com.example.foodorder.data.entity.Foods
+import com.example.foodorder.room.FavDao
+
+class LocalFoodDataSource(private val favDao: FavDao) {
+
+    suspend fun insertFavFoods(food: Foods) = favDao.insertFavFoods(food)
+
+    suspend fun deleteFavFoods(foodId:Int) = favDao.deleteFavFoods(foodId)
+
+    suspend fun getFavFoods(): List<Foods> = favDao.getFavFoods()
+
+    suspend fun isFavorite(foodId: Int): Int = favDao.isFavorite(foodId)
+
+
 }
