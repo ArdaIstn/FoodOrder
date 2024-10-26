93a1879c6b119ec1fe1f209480b47ecaf9d27fcf
Arda <arda_isitan55@hotmail.com>
2024-10-26 22:20:12 +0300

    Refreshed favorite foods list in onResume to displayed updated data on fragment return.

--- app/src/main/java/com/example/foodorder/ui/fragments/FavFragment.kt
+++ app/src/main/java/com/example/foodorder/ui/fragments/FavFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foodorder.R
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.foodorder.databinding.FragmentFavBinding
+import com.example.foodorder.ui.adapter.FavFoodListAdapter
+import com.example.foodorder.ui.viewmodel.FavViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,5 +33,10 @@ class FavFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        favViewModel.getFavFoods()
     }
 }
