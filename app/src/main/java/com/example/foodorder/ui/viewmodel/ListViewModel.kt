9aaf0299575c2b3be8da800077594a22fd2738b9
Arda <arda_isitan55@hotmail.com>
2024-10-26 20:47:53 +0300

    Cleaned Code in ListViewModel.

--- app/src/main/java/com/example/foodorder/ui/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/foodorder/ui/viewmodel/ListViewModel.kt
@@ -1,24 +1,18 @@
 package com.example.foodorder.ui.viewmodel
 
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.foodorder.data.entity.Foods
 import com.example.foodorder.data.repository.FoodRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
-
 
 @HiltViewModel
 class ListViewModel @Inject constructor(private val foodsRepository: FoodRepository) : ViewModel() {
 
     private val _foodsList = MutableLiveData<List<Foods>>()
-    val foodsList: MutableLiveData<List<Foods>>
-        get() = _foodsList
+    val foodsList: LiveData<List<Foods>> = _foodsList
+
 
     private var allFoods: List<Foods> = listOf()
 
@@ -29,18 +23,14 @@ class ListViewModel @Inject constructor(private val foodsRepository: FoodReposit
 
     private fun fetchFoods() {
         viewModelScope.launch {
-            val foodsList = withContext(Dispatchers.IO) {
-                foodsRepository.getAllFoods()
-            }
-            allFoods = foodsList
-            _foodsList.value = foodsList
-
+            allFoods = foodsRepository.getAllFoods()
+            _foodsList.value = allFoods
         }
     }
 
 
     fun filterFoods(query: String) {
-        val filteredList = if (query.isEmpty()) {
+        _foodsList.value = if (query.isEmpty()) {
             allFoods
         } else {
             allFoods.filter {
